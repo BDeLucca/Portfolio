965f0bced6a8602fbcdb574f1ecef74ee27b51bf
Bruno DeLucca <bruno@delucca.cl>
2020-10-23 14:49:47 +0200

    Updates

---
+++ precache-manifest.9e1f782acc3e8c9ff367c586de0c175c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aef8bba8b5eac7dbc9035dd49ce3efb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dda008eb0655bc6e22ed",
+    "url": "/Portfolio/static/css/main.da084c8a.chunk.css"
+  },
+  {
+    "revision": "cbdae0a44f0ab3c607e4",
+    "url": "/Portfolio/static/js/2.44a81c96.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.44a81c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda008eb0655bc6e22ed",
+    "url": "/Portfolio/static/js/main.24f9c388.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "4bfeed38f28bb8054df0467687de1846",
+    "url": "/Portfolio/static/media/AboutMyself.4bfeed38.jpg"
+  },
+  {
+    "revision": "bb7742f942cc30d3e919f1a2444f34bf",
+    "url": "/Portfolio/static/media/About_Me.bb7742f9.jpg"
+  },
+  {
+    "revision": "f5b77347841c4f2f92ef09a99c64f424",
+    "url": "/Portfolio/static/media/Click-Click-Print.f5b77347.png"
+  },
+  {
+    "revision": "03cbda92b727f52aa43deb2156fef2f7",
+    "url": "/Portfolio/static/media/MainPhoto.03cbda92.png"
+  },
+  {
+    "revision": "ecd566035195f9b0a8dc46cec61361b7",
+    "url": "/Portfolio/static/media/NoSQL.ecd56603.jpg"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/Portfolio/static/media/SQL.21812f6d.png"
+  },
+  {
+    "revision": "363dbb34fd7eaf4476d2de0d74d2d908",
+    "url": "/Portfolio/static/media/html-css.363dbb34.png"
+  }
+]);
